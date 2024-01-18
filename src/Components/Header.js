fb8a14b3d01c578790e803240de7f0087973a258
Costea-B <155655464+Costea-B@users.noreply.github.com>
2024-01-19 01:15:18 +0200

    build(routes)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Routes, Route, Link} from 'react-router-dom'
+import About from '../pages/About'
+import Blogs from '../pages/Blogs'
+import Home from '../pages/Home'
+
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <Link to="/">Acasă</Link>
+        <Link to="/blogs">Bloguri</Link>
+        <Link to="/about">Despre noi</Link>
+      </div>
+      <Routes>
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="about" element={<About />} />
+        </>
+      </Routes>
+    </div>
   )
 }
---
+++ src/pages/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+    return <h1>Despre noi</h1>;
+  };
+  
+  export default About;
